fca94e1b4cf52d8b42dd115b6cab0d9547666f4c
Tim Wolfram <77680127+timwolfram1994@users.noreply.github.com>
2023-06-02 10:03:49 +0200

    PDB_to_graph function created

--- Greate Graph.py
+++ Greate Graph.py
@@ -1,53 +1,57 @@
 import networkx as nx
-import biopandas
 import os
 from biopandas.pdb import PandasPdb
 from IPython.display import display
 import matplotlib.pyplot as plt
 
-path = "D:/Eigene Dateien/Eigene Dokumente/Uni/Module/fortgeschrittene Methoden/PDB_files/2eso.pdb"
+filename = "2eso.pdb"
+path = os.path.join(os.getcwd(), filename)
 
 
 #####chatGPT####
 
+def pdb_to_graph(path):
+    # Load the PDB file using Biopandas
+    df = PandasPdb().read_pdb(path)
 
-# Load the PDB file using Biopandas
-df = PandasPdb().read_pdb(path)
+    # Create a NetworkX graph
+    G = nx.MultiGraph()
 
-# Create a NetworkX graph
-G = nx.MultiGraph()
+    # Iterate over the atoms in the DataFrame
+    for i, row in df.df['ATOM'].iterrows():
+        atom_serial = row['atom_number']
+        atom_element = row['element_symbol']
 
-# Iterate over the atoms in the DataFrame
-for i, row in df.df['ATOM'].iterrows():
-    atom_serial = row['atom_number']
-    atom_element = row['element_symbol']
+        # Add nodes to the graph for each atom
+        G.add_node(atom_serial, element=atom_element)
 
-    # Add nodes to the graph for each atom
-    G.add_node(atom_serial, element=atom_element)
+        # Add edges to the graph between adjacent atoms in the residue
+        residue_id = row['residue_number']
+        chain_id = row['chain_id']
 
-    # Add edges to the graph between adjacent atoms in the residue
-    residue_id = row['residue_number']
-    chain_id = row['chain_id']
+        if i > 0:
+            prev_row = df.df['ATOM'].iloc[i - 1]
+            prev_residue_id = prev_row['residue_number']
+            prev_chain_id = prev_row['chain_id']
 
-    if i > 0:
-        prev_row = df.df['ATOM'].iloc[i - 1]
-        prev_residue_id = prev_row['residue_number']
-        prev_chain_id = prev_row['chain_id']
+            if (residue_id == prev_residue_id) and (chain_id == prev_chain_id):
+                prev_atom_serial = prev_row['atom_number']
 
-        if (residue_id == prev_residue_id) and (chain_id == prev_chain_id):
-            prev_atom_serial = prev_row['atom_number']
+                # Calculate bond type based on interatomic distance
+                distance = ((row['x_coord'] - prev_row['x_coord']) ** 2 +
+                            (row['y_coord'] - prev_row['y_coord']) ** 2 +
+                            (row['z_coord'] - prev_row['z_coord']) ** 2) ** 0.5
 
-            # Calculate bond type based on interatomic distance
-            distance = ((row['x_coord'] - prev_row['x_coord']) ** 2 +
-                        (row['y_coord'] - prev_row['y_coord']) ** 2 +
-                        (row['z_coord'] - prev_row['z_coord']) ** 2) ** 0.5
+                bond_type = 1
+                if distance <= 1.5:  # Adjust this threshold for your specific case
+                    bond_type = 2
 
-            bond_type = 1
-            if distance <= 1.5:  # Adjust this threshold for your specific case
-                bond_type = 2
+                G.add_edge(atom_serial, prev_atom_serial, bond_type)
+    return G
 
-            G.add_edge(atom_serial, prev_atom_serial, bond_type)
 
+
+G = pdb_to_graph(path)
 
 
 # Draw the graph
