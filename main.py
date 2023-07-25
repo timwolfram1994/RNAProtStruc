32f8962dcf53362cfba9392d0df3bd21679ab4bc
Dertalex <kleinstadttauber@gmail.com>
2023-07-25 12:11:16 +0200

    as of 06.02. 10:50

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+from matplotlib import pyplot as plt
 from create5Ggraph import *
 from pdb_to_graph import *
+from pebblegame import *
 import scipy as sp
 # list of edges
-l = [(0, 1), (1, 2), (2, 3), (3, 0), (0, 1), (0, 2), (1, 3)]
+l = [(0, 1), (1, 2), (2, 3), (3, 0), (1, 0), (0, 2), (1, 3)]
 
 multigraph1G = nx.MultiGraph(l)
 
 
-filename = "2eso.pdb"
-path = os.path.join(os.getcwd(),"pdb_samples", filename)
-test = pdb_to_graph(path)
+# filename = "2eso.pdb"
+# path = os.path.join(os.getcwd(),"pdb_samples", filename)
+test = multigraph1G #pdb_to_graph(path)
+test5g = create5Ggraph(test)
+# nx.draw(test, with_labels=True)
+# plt.show()
+supportgraph = create_initialsupportGraph(test5g)
+print(supportgraph.nodes)
+# toprint = test5g.edges
+# for edge in toprint:
+#     print(edge)
 
-nx.draw(test, with_labels=True)
-plt.show()
 
 # print(multigraph.edges)
 
--- pebblegame.py
+++ pebblegame.py
@@ -1,6 +1,29 @@
 import networkx as nx
 
-def create_auxillaryGraph(multigraph5G):
-    pass
+def create5Ggraph(multigraph1G):
+    # create an edge list for the 5G Graph
+    listofedges = []
+    for edge in multigraph1G.edges:
+        for i in range(0, 5):
+            listofedges.append((edge[0], edge[1]))
+
+    # append all edges from edge list to a new 5G Graph (not mattering about too many edges)
+    # to achieve an edge list with (x,x,x) tuples
+    multigraph5G = nx.MultiGraph(listofedges)
+    listofedges.clear()
+    for tuple in multigraph5G.edges:
+        listofedges.append(tuple)
+
+    # create a sober edge list and the correct 5G Graph
+    edgelist5G = [x for x in listofedges if x[2] < 6]
+    multigraph5G = nx.MultiGraph(edgelist5G)
+
+    return multigraph5G
+
+def create_initialsupportGraph(multigraph5G):
+    h = nx.MultiGraph()
+    h.add_nodes_from(multigraph5G)
+    return h
+
 def pebblegame(fiveGgraph):
     pass
